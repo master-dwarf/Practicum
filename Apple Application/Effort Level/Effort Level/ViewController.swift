a09534b8b2df0054dc4531d1d25701454dbe5935
Grady Hilgendorf <rockenswim@yahoo.com>
2018-06-06 11:07:32 -0500

    work on apple application
    worked on converting the time input into different segments of time

--- Apple Application/Effort Level/Effort Level/ViewController.swift
+++ Apple Application/Effort Level/Effort Level/ViewController.swift
@@ -9,6 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Elements declatation
+    @IBOutlet weak var timeText: UITextField!
+    @IBOutlet weak var distanceText: UITextField!
+    @IBOutlet weak var enteredTime: UITextField!
+    @IBOutlet weak var enteredDistace: UITextField!
+    @IBOutlet weak var colorStack:UIStackView!
+    @IBOutlet weak var timeStack:UIStackView!
+    
+    //Private variables
+    private var minutes:Int = 0
+    private var seconds:Int = 0
+    private var hundreths:Int = 0
+    private var timeBool: Bool = false
+    private var distance:Int = 0
+    private var distanceBool: Bool = false
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +35,51 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func checkInput(){
+        if(timeText.text != ""){
+            timeBool = true;
+            timeText.textColor = UIColor.black
+        }
+        else{
+            timeText.textColor = UIColor.red
+        }
+        if(distanceText.text != ""){
+            distanceBool = true;
+            distanceText.textColor = UIColor.black
+        }
+        else{
+            distanceText.textColor = UIColor.red
+        }
+    }
+    
+    func parseTime(){
+        top: if(timeBool == false || distanceBool == false){
+            break top
+        }
+        else{
+            let str = timeText.text
+            let count = str?.count
+            if(count! < 6 ){
+                //time is less than 1 min
+                let sub1 = str?.split(separator: ".")
+                seconds = Int(String(sub1![0]))!
+                hundreths = Int(String(sub1![1]))!
+            }
+            else{
+                //time is more than 1 min
+                let sub1 = str?.split(separator: ".")
+                minutes = Int(String(sub1![0]))!
+                seconds = Int(String(sub1![1]))!
+                hundreths = Int(String(sub1![2]))!
+            }
+        }
+    }
+    
+    @IBAction func buttonClick(sender: UIButton) {
+        checkInput()
+        parseTime()
     }
 
 
